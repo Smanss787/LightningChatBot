c01229b1f33b789bb9f1d2b3a49dab0e6203dd6d
smael.tanrhori <smael.tanrhori@FR-L3356930.groupinfra.com>
2018-12-04 16:57:42 +0100

    time added

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,4 +1,18 @@
-var $firstName ="Moi";
+var $firstName ="Prenom";
+var $lastNAme = "Nom"
+function getdate(){
+	var currentdate = new Date();
+	var datetime = currentdate.getDate() + "/"
+    + (currentdate.getMonth()+1)  + "/" 
+    + currentdate.getFullYear() + " at "  
+    + currentdate.getHours() + ":"  
+    + currentdate.getMinutes() + ":" 
+    + currentdate.getSeconds();
+	return datetime;
+}
+
+
+
 function connect() {
 	var socket = new SockJS('/chat-messaging');
 	stompClient = Stomp.over(socket);
@@ -13,10 +27,12 @@ function connect() {
 
 function draw(side, text, firstName) {
 	console.log("drawing...");
+	var $date = getdate();
     var $message;
     $message = $($('.message_template').clone().html());
     $message.addClass(side).find('.text').html(text);
     $message.addClass(side).find('.name').html($firstName+" : ");
+    $message.addClass(side).find('.date').html($date);
     $('.messages').append($message);
     return setTimeout(function () {
         return $message.addClass('appeared');
@@ -27,8 +43,10 @@ function disconnect(){
 	stompClient.disconnect();
 }
 function sendMessage(){
+	var $date = getdate();
 	stompClient.send("/app/message", {}, JSON.stringify({'message': $("#message_input_value").val(),
-														 "from" : $firstName
+														 'from' : $firstName,
+														 'date' : $date
 														 }));
 
 }
